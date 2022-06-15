9a4b4f74d40097c6db22bc154bbd3c58ce660f04
Adinan Batista Alves <adinan0803@gmail.com>
2022-06-15 11:23:59 -0300

    parametrizando o prefixo da rota da API

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -38,7 +38,7 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
-            Route::prefix('contratos/api')
+            Route::prefix(config('app.prefix', '').'api')
                 ->middleware('api')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
