3e3c499f57b9c05b085e6b2bd3118f4b89e63459
Adinan Batista Alves <adinan0803@gmail.com>
2022-06-15 11:53:58 -0300

    corrigindo o prefixo da rota da API

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -38,7 +38,7 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
-            Route::prefix(env('APP_FOLDER', 'contratos').'api')
+            Route::prefix(env('API_FOLDER', 'contratos/').'api')
                 ->middleware('api')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
